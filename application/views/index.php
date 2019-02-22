2aa529195ebd70c4e76273a754f946f5767ed369
Angel Garcia Rodriguez <angelrodriguez@ucol.mx>
2019-02-22 11:54:49 -0600

    Again

--- application/views/index.php
+++ application/views/index.php
@@ -4,10 +4,118 @@
 <div class="row bar-gray">
 	<div class="container padding-box">
 		<div class="row">
-			<div class="col-sm-5 offset-sm-1">
-				<h5>¿Tienes un proyecto en mente? Contactanos!</h5>
+			<div class="col-sm-9 offset-sm-1">
+				<h5 class="float-left">¿Tienes un proyecto en mente? Contactanos!</h5>
+				<a href="" class="btn btn-gray float-right">Enviar</a>
 			</div>
-			<div class="col-sm-3"><a href="" class="btn float-right">Enviar</a></div>
+			<br><br>
+			<div class="col-sm-10 offset-sm-1">
+				<div class="row">
+					<div class="col-sm-3">
+						<label for="" class="col-sm-5 float-left">Nombre</label>
+						<input type="text" class="col-sm-7 form-control float-right" style="border-bottom: 1px solid #aaa; background: transparent;">
+					</div>
+					<div class="col-sm-3">
+						<label for="" class="col-sm-5 float-left">Correo</label>
+						<input type="text" class="col-sm-7 form-control float-right" style="border-bottom: 1px solid #aaa; background: transparent;">
+					</div>
+					<div class="col-sm-3">
+						<label for="" class="col-sm-5 float-left">Télefono</label>
+						<input type="text" class="col-sm-7 form-control float-right" style="border-bottom: 1px solid #aaa; background: transparent;">
+					</div>
+					<div class="col-sm-3">
+						<label for="" class="col-sm-5 float-left">Empresa</label>
+						<input type="text" class="col-sm-7 form-control float-right" style="border-bottom: 1px solid #aaa; background: transparent;">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-5">
+				<img  style="width:100%;" src="<?php echo base_url().'assets/images/imagen_home_1.png' ?>" alt="">
+			</div>
+			<div class="col-sm-7 bar-orange" style="padding: 30px;">
+				<div class="row">
+					<div class="col-sm-12">
+						<h5 class="text-white float-left">Servicios</h5>
+						<ul class="list-dot float-right">
+							<li>
+								
+							</li>
+							<li>
+								
+							</li>
+							<li>
+								
+							</li>
+							<li>
+								
+							</li>
+						</ul>
+					</div>
+					<br><br>
+					<div class="col-sm-12">
+						<h3 class="text-center text-white">Desarrollo de productos integrales</h3>
+						<br>
+						<p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores rem amet voluptatem numquam tempora aut distinctio impedit ullam earum recusandae eveniet accusamus commodi, ut provident repellat corporis, reiciendis, deserunt, necessitatibus?</p>
+					</div>
+					<div class="col-sm-12">
+						<p class="text-right">
+							<a href="" class="btn btn-white">Ver más</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<br>
+		<div class="row">
+			<div class="col-sm-12">
+				<h4 class="">Proyectos</h4>
+			</div>	
+			<div class="col-3" style="background:#fff;">
+				<img style="max-width:100%;" src="<?php echo base_url().'assets/images/Proyecto_casajaver_Home.png' ?>" alt="">
+				<h5>Proyecto</h5>
+				<div class="row">
+					<div class="col-sm-12">
+						<p class="text-muted float-left">Desarrollo Integral</p>
+						<a href="" class="btn-app-outline float-right">Ver</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-3" style="background:#fff;">
+				<img style="max-width:100%;" src="<?php echo base_url().'assets/images/Proyecto_ayuntamiento_Home.png' ?>" alt="">
+				<h5>Proyecto</h5>
+				<div class="row">
+					<div class="col-sm-12">
+						<p class="text-muted float-left">Desarrollo Integral</p>
+						<a href="" class="btn-app-outline float-right">Ver</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-3" style="background:#fff;">
+				<img style="max-width:100%;" src="<?php echo base_url().'assets/images/Proyecto_mugerza_Home.png' ?>" alt="">
+				<h5>Proyecto</h5>
+				<div class="row">
+					<div class="col-sm-12">
+						<p class="text-muted float-left">Desarrollo Integral</p>
+						<a href="" class="btn-app-outline float-right">Ver</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-3" style="background:#fff;">
+				<img style="max-width:100%;" src="<?php echo base_url().'assets/images/Proyecto_zigzac_Home.png' ?>" alt="">
+				<h5>Proyecto</h5>
+				<div class="row">
+					<div class="col-sm-12">
+						<p class="text-muted float-left">Desarrollo Integral</p>
+						<a href="" class="btn-app-outline float-right">Ver</a>
+					</div>
+				</div>
+			</div>	
 		</div>
 	</div>
 </div>
